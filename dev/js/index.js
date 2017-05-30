dee65851885c92df9f4f57ffcc6bba85153267cf
sturloly <sturloly@gmail.com>
2017-05-31 02:08:02 +0800

    setup 1

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,5 +1,9 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
+import {createStore} from 'redux'; //"{} === var createStore = redux.createStore... "
+
+const store = crateStore();
+
 
 ReactDOM.render(<h1> Hello HI</h1>, document.getElementById('root'));
