1c3c720639233d28f53199920a3179c69428ebe4
sturloly <sturloly@gmail.com>
2017-05-31 03:37:08 +0800

    mapStateToProps

--- dev/components/app.js
+++ dev/components/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
-require ('../scss/style.scss');
+import UserList from '../containers/user-list'
+require('../scss/style.scss');
 
 const App = () => (
   <div>
       <h2>用户列表:</h2>
-      <hr/> 
+      <UserList/>
+      <hr/>
       <h2>用户信息:</h2>
   </div>
 );
